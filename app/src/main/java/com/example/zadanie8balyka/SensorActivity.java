bee46896be30a0af9db54b77cff513b28b609669
IgoreSHik <balyko070699@gmail.com>
2021-11-29 09:54:12 +0100

    Initial commit

---
+++ app/src/main/java/com/example/zadanie8balyka/SensorActivity.java
@@ -0,0 +1,27 @@
+package com.example.zadanie8balyka;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+
+import java.util.List;
+
+public class SensorActivity extends AppCompatActivity {
+
+    private SensorManager sensorManager;
+    private List<Sensor> sensorList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sensor_activity);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
+    }
+
+
+}
